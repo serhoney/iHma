801234a54ab5c8aa9f9d577867b9fcb2c4695bfe
serhoney <44899005+serhoney@users.noreply.github.com>
2019-06-27 16:40:37 +0700

    Beta Version

--- Develop_App/src/app/app.component.ts
+++ Develop_App/src/app/app.component.ts
@@ -2,49 +2,60 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+//import { AdmobProvider } from '../providers/admob/admob';
+import { LandingPage } from '../pages/landing/landing';
 
-import { HomePage } from '../pages/home/home';
-import { DashboardPage } from '../pages/dashboard/dashboard'
-import { FCM } from '@ionic-native/fcm';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = DashboardPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,public fcm: FCM) {
+  // rootPage: any = LandingPage;
+
+  rootPage: any;  // <<< Do not set the root page until the platform.ready()
+                  //      This avoids to face the plugin loading error.
+
+
+  constructor(public platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    //public admobProvider: AdmobProvider
+    ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      this.rootPage = LandingPage;
       statusBar.styleDefault();
       splashScreen.hide();
 
-      if(platform.is('cordova')){
-        this.initApp();
-    }
+      /*
+      if (this.platform.is('ios') || this.platform.is('android')) {
+
+        // Show Video Ad After 1 Minutes
+        setInterval(() => {
+          this.prepareAdmobVideo();
+        }, 120000);
+
+        // Show Interstitial Ad After 30 Sec
+        setInterval(() => {
+          this.prepareInterstitial();
+        }, 60000);
+      }
+      */
     });
   }
-  
-  initApp(){
 
-    this.fcm.subscribeToTopic('senchabox_broadcast');
+  /**
+   * Prepare and show admob Video ad
 
-    this.fcm.getToken().then(token => {
-    alert(token);
-    });
+  prepareAdmobVideo() {
+    this.admobProvider.prepareAdmobVideo();
+  }
+ */
+  /**
+   * Prepare and show admob Interstitial Ad
 
-    this.fcm.onNotification().subscribe(data => {
-    if(data.wasTapped){
-        console.log("Received in background");
-    } else {
-        console.log("Received in foreground");
-    }
-    });
-
-    this.fcm.onTokenRefresh().subscribe(token => {
-    alert(token);
-    });
-
+  prepareInterstitial() {
+    this.admobProvider.prepareInterstitial();
+  }*/
 }
-}
-
